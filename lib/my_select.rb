cecbe0743fb83738d369f8e06d96517a8323dbd6
Alex Bujenita <dekc89@gmail.com>
2018-12-09 19:09:08 +0000

    select simulator

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(collection)
- # your code here!
+def my_select input_array
+  i = 0
+  final = []
+  while i < input_array.length
+    final.push(input_array[i]) if yield(input_array[i])
+    i+=1
+  end
+  final
 end
