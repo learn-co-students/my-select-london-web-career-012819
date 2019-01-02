83e485f07124ea45ae2d795dfe5b6ea16ca7a51e
stubrew24 <sewelly@gmail.com>
2019-01-02 20:45:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,8 +4,8 @@ def my_select(collection)
  i = 0
  array = []
  while i < collection.length
-   if yield(collection[i]) == true
-     array.push(collection[i])
+   if yield(collection[i])
+     array << collection[i]
    end
    i += 1
  end
